6b75bee3b27ed40628b6924159705141a51565b7
Ingsha Hang Subba <ingshalimbu09@gmail.com>
2025-09-20 01:40:18 +0530

    Add advanced working tool pages

---
+++ Calculator.jsx
@@ -0,0 +1,13 @@
+// Calculator component
+import React from 'react';
+
+const Calculator = () => {
+    return (
+        <div>
+            <h1>Calculator</h1>
+            {/* Calculator implementation */}
+        </div>
+    );
+};
+
+export default Calculator;
